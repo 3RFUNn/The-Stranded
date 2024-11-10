0a33c2335c2207553363f2d9887a7ba55d1c4f17
OykuPam <128751530+PamOyku@users.noreply.github.com>
2024-11-10 22:59:55 +0000

    Added the code for health bar and player health

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    public Slider slider;
+
+    private float currentDisplayedHealth;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentDisplayedHealth = slider.value;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentDisplayedHealth != slider.value)
+        {
+            currentDisplayedHealth = Mathf.Lerp(currentDisplayedHealth, slider.value, Time.deltaTime * 10);
+
+            slider.value = currentDisplayedHealth;
+        }
+
+    }
+
+    public void SetMaxHealth( int health)
+    {
+        slider.maxValue = health;
+
+        slider.value = health;
+
+        currentDisplayedHealth = health;
+    }
+
+
+    public void SetHealth( int health)
+    {
+        slider.value = health;
     }
 }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+
+    public int maxHealth = 100;
+    private int currentHealth;
+    public HealthBar healthBar;
+
     // Start is called before the first frame update
     void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    public void TakeDamage( int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+
+        if (currentHealth <= 0 )
+        {
+            Die();
+        }
+    }
+
+    void Die()
     {
 
     }
