802e1f7d83058339d6c713c49f72b7f96553a20e
3RFUNn <68325981+3RFUNn@users.noreply.github.com>
2025-01-03 14:39:45 +0000

    Update EnemyHealth.cs

--- Assets/EnemyHealth.cs
+++ Assets/EnemyHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UIElements;
 
 public class EnemyHealth : MonoBehaviour
@@ -10,6 +11,14 @@ public class EnemyHealth : MonoBehaviour
     private int currentHealth;
     public HealthBar healthBar;
     [SerializeField] private GameObject parent;
+    private EnemyLoot loot;
+    
+    public GameObject resourcePrefab;
+    public GameObject healthTokenPrefab;
+    public Animator animator;
+    [SerializeField] private MonoBehaviour script;
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Rigidbody rb;
 
 
     // Start is called before the first frame update
@@ -28,8 +37,57 @@ public class EnemyHealth : MonoBehaviour
 
     void CheckAndUpdate(){
         if(currentHealth <= 0){
-            Destroy(parent);
+            TriggerAnimationAndDropLoot();
         }
+    }
+    
+    
+    public void DropLoot()
+    {
+        GameObject itemToDrop = null;
+
+        switch (parent.tag)
+        {
+            case "Melee":
+                itemToDrop = resourcePrefab;
+                break;
+            case "Coward":
+                itemToDrop = healthTokenPrefab;
+                break;
+        }
+
+        if (itemToDrop != null)
+        {
+            Instantiate(itemToDrop, transform.position, Quaternion.identity);
+        }
+
+        Destroy(parent); // Destroy the enemy game object
+    }
+
+    public void TriggerAnimationAndDropLoot()
+    {
+        StartCoroutine(TriggerAnimationAndDropLootCoroutine());
+    }
+
+    private IEnumerator TriggerAnimationAndDropLootCoroutine()
+    {
+        animator.SetBool("IsDead", true);
+        FreezeAndDecreaseHeight();
+        script.enabled = false;
+        agent.enabled = false;
+        yield return new WaitForSeconds(3f);
+        DropLoot();
+    }
+    
+    public void FreezeAndDecreaseHeight()
+    {
+        // Freeze position and rotation
+        rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+
+        // Decrease height by 5 units
+        Vector3 newPosition = parent.transform.position;
+        newPosition.y -= 1;
+        parent.transform.position = newPosition;
     }
 
 }
