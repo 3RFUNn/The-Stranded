561b2e0fe8e0f0f0db82540fa12a41d9305af796
Mohammad Inshaul Haque <inshaulhaque@gmail.com>
2025-01-06 00:07:52 +0000

    Replaced character + Beautified Puzzle 2

--- Assets/Scripts/Managers/GamePhaseManager.cs
+++ Assets/Scripts/Managers/GamePhaseManager.cs
@@ -58,6 +58,9 @@ public class GamePhaseManager : MonoBehaviour
 
     public EnergyCharger EnergyCharger;
 
+    public GameObject characterMesh;
+
+
 
     private void Awake() {
         instance = this;
@@ -87,6 +90,7 @@ public class GamePhaseManager : MonoBehaviour
             BGM.Play();
             enemySpawner.InitiateEnemies();
             ApplySavedDifficulty();
+            characterMesh.SetActive(false);
         } else{
             //hide main menu and directly start game
             MainMenu.SetActive(false);
@@ -95,6 +99,7 @@ public class GamePhaseManager : MonoBehaviour
             PlayPostCutSceneBGM();
             enemySpawner.InitiateEnemies();
             ApplySavedDifficulty();
+            characterMesh.SetActive(true);
         }
 
         if (StartWithTablet){
@@ -157,6 +162,7 @@ public class GamePhaseManager : MonoBehaviour
 
         //switch input action map
         Input.SwitchCurrentActionMap("Player");
+
     }
 
     //freeze time, display cursor, disable in-game inputs
@@ -215,12 +221,16 @@ public class GamePhaseManager : MonoBehaviour
     public void SwitchCutscene(bool isInCutscene){
         if (isInCutscene) {
             HUD.SetActive(false);
+            characterMesh.SetActive(false);
             Input.enabled = false;
         }
         else { 
             HUD.SetActive(true);
             Input.enabled = true;
-            
+            enemySpawner.InitiateEnemies();
+            ApplySavedDifficulty();
+            PlayPostCutSceneBGM();
+            characterMesh.SetActive(true);
         }
     }
 
